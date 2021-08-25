f684aaf10f4328a552e929c7b244210171a4b59a
ranicholson <ranicho2@gmail.com>
2021-08-25 11:03:05 -0500

    Checker problems

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,38 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: Head of list to check.
+ * Return:1 if it is a palindrome, otherwise 0.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *listCycler = *head;
+	int listCount = 0;
+	int listArray[2000];
+
+	if (listCycler == NULL)
+		return (1);
+
+	while (listCycler)
+	{
+		listArray[listCount] = listCycler->n;
+		listCycler = listCycler->next;
+		listCount++;
+	}
+
+	listCycler = *head;
+
+	while (listCycler)
+	{
+		if (listCycler->n != listArray[listCount - 1])
+			return (0);
+
+		listCount--;
+		listCycler = listCycler->next;
+	}
+
+	return (1);
+}
