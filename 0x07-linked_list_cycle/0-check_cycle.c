42707c529caa4ba22bcc0e4f91b58905394b75fe
ranicholson <ranicho2@gmail.com>
2021-09-06 09:40:01 -0500

    Task 0 file, README, and header file

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+
+/**
+ * check_cycle - checks if linked list contains a cycle.
+ * @list: linked list to check
+ * Return: 0 if there is no cycle or 1 if there is.
+*/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *steadyChecker = list;
+	listint_t *speedyChecker = list->next;
+
+	while (speedyChecker->next != NULL)
+	{
+		speedyChecker = speedyChecker->next;
+
+		if (speedyChecker->next != NULL)
+			speedyChecker = speedyChecker->next;
+
+		steadyChecker = steadyChecker->next;
+
+		if (steadyChecker == speedyChecker)
+			return (1);
+	}
+
+	return (0);
+}
---
+++ 0x07-linked_list_cycle/README.md
@@ -0,0 +1,11 @@
+# 0x07. Linked list cycle
+
+Checking a singly linked list for cycles.
+
+## How can you practice this same project?
+
+Take a look at the AR/VR specialization at Holberton.
+
+## Questions or Comments
+
+[Allen Nicholson](https://github.com/ranicholson)
