14f0deb16566442ad8b4d8673569ea1d2a1156ac
ranicholson <ranicho2@gmail.com>
2021-09-22 12:01:49 -0500

    Task 0 and README

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Method to determine if dataset is a valid UTF-8 encoding"""
+
+
+def validUTF8(data):
+    """Checks for valid UTF-8 encoding"""
+    count = 0
+    for item in data:
+        if count == 0:
+            if (item >> 5) == 0b110:
+                count = 1
+            elif (item >> 4) == 0b1110:
+                count = 2
+            elif (item >> 3) == 0b11110:
+                count = 3
+            elif (item >> 7):
+                return False
+        else:
+            if (item >> 6) != 0b10:
+                return False
+            count -= 1
+        if count > 0:
+            return False
+
+        return True
---
+++ 0x09-utf8_validation/README.md
@@ -0,0 +1,11 @@
+# 0x09. UTF-8 Validation
+
+Checking if a dataset is properly UTF-8 encoded.
+
+## How can you practice this same project?
+
+Take a look at the AR/VR specialization at Holberton.
+
+## Questions or Comments
+
+[Allen Nicholson](https://github.com/ranicholson)
