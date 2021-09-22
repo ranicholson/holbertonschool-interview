6ecb80fe3ab8ca2d5b0d995faa636c998f1db330
ranicholson <ranicho2@gmail.com>
2021-09-22 12:42:46 -0500

    Task 0 updates

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,15 +6,16 @@ def validUTF8(data):
     """Checks for valid UTF-8 encoding"""
     count = 0
     for item in data:
+        item = item & 255
         if count == 0:
-            if (item >> 5) == 0b110:
-                count = 1
-            elif (item >> 4) == 0b1110:
-                count = 2
-            elif (item >> 3) == 0b11110:
-                count = 3
-            elif (item >> 7 == 0):
+            if (item >> 7) == 0:
                 count = 0
+            elif (item >> 5) == 6:
+                count = 1
+            elif (item >> 4) == 14:
+                count = 2
+            elif (item >> 3) == 30:
+                count = 3
             else:
                 return False
         else:
