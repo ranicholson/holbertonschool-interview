5c05e9d0e98c137392aba746b899e7548cc9a528
ranicholson <ranicho2@gmail.com>
2021-11-03 10:44:59 -0500

    Task 0 and README

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,52 @@
+#include "search.h"
+
+/**
+ * linear_skip - function to search skip list
+ * @list: list to search
+ * @value: value to search for
+ * Return: Pointer to node where value is located or NULL if something is off
+ */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *slowLane = list;
+	skiplist_t *expressLane = list->express;
+	int foundFlag = 0, expressFlag = 0;
+
+	if (!list)
+		return (NULL);
+	while (slowLane)
+	{
+		if (expressLane->n < value && expressFlag == 0)
+		{
+			if (expressLane->express == NULL)
+			{
+				slowLane = expressLane;
+				while (expressLane->next != NULL)
+					expressLane = expressLane->next;
+				expressFlag = 1;
+			}
+			else
+			{
+				printf("Value checked at index [%lu] = [%d]\n",
+				 expressLane->index, expressLane->n);
+				slowLane = expressLane;
+				expressLane = expressLane->express;
+				continue;
+			}
+		}
+		if (foundFlag == 0)
+		{
+			printf("Value found between indexes [%lu] and [%lu]\n",
+			 slowLane->index, expressLane->index);
+			foundFlag = 1;
+		}
+		printf("Value checked at index [%lu] = [%d]\n",
+		 slowLane->index, slowLane->n);
+
+		if (value == slowLane->n)
+			return (slowLane);
+		slowLane = slowLane->next;
+	}
+	return (NULL);
+}
---
+++ 0x0E-linear_skip/README.md
@@ -0,0 +1,11 @@
+# 0x0E. Linear search in skip list
+
+Creating a function to search a skip list.
+
+## How can you practice this same project?
+
+Take a look at the AR/VR specialization at Holberton.
+
+## Questions or Comments
+
+[Allen Nicholson](https://github.com/ranicholson)
