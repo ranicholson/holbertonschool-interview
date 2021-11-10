97ab505a25843d87de66d3582c41076a49006d22
ranicholson <ranicho2@gmail.com>
2021-11-10 13:38:56 -0600

    Semistandard fixes

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const movieNumber = parseInt(process.argv[2]);
-const url = 'https://swapi-api.hbtn.io/api/films/' + movieNumber +'/';
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieNumber + '/';
 
 request(url, function (error, response, body) {
   if (error) {
@@ -12,18 +12,18 @@ request(url, function (error, response, body) {
     const characterList = {};
 
     bodyShot.forEach(character => {
-    request(character, function (error, response, body) {
-      if (error) {
-        console.log(error);
-      }
-      const name = JSON.parse(body).name;
-      characterList[character] = name;
-      if (Object.values(characterList).length === bodyShot.length) {
-        bodyShot.forEach(character => {
-          console.log(characterList[character]);
-        });
-      }
+      request(character, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        }
+        const name = JSON.parse(body).name;
+        characterList[character] = name;
+        if (Object.values(characterList).length === bodyShot.length) {
+          bodyShot.forEach(character => {
+            console.log(characterList[character]);
+          });
+        }
+      });
     });
-  });
   }
 });
